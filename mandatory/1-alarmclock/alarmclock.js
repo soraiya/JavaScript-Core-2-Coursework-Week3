4445a7f747e25066116684ac182c55b61acff901
soraiya <soraiyamm@gmail.com>
2021-03-05 21:16:54 +0000

    Alarm clock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+function setAlarm() {
+  let inputBox = document.getElementById("alarmSet");
+  let timer = document.getElementById("timeRemaining");
+  document.getElementById("set").disabled = true;
+  if (parseInt(inputBox.value) <= 0) {
+    return 
+  } 
+  let setTimer = setInterval(function() {
+    //decrementing timer
+    inputBox.value--;
+    //changing the timer
+    timer.innerText = "Time Remaining: " + "00:" + inputBox.value;
+    //add a conditional for the timer to stop at 0
+    if (inputBox.value === "0") {
+      clearInterval(setTimer)
+      // play alarm sound at 0
+      playAlarm(); 
+      document.getElementById("set").disabled = false;
+}
+  }, 1000); 
+
+
+
+
+
+}
+
+
 
 // DO NOT EDIT BELOW HERE
 
